f5f2c53691bdd85c5d126dc87ae273e7b384bccf
Aditya Bhargava <bluemangroupie@gmail.com>
2014-02-21 16:42:00 -0800

    better help message and write out to a custom file

--- src/Main.hs
+++ src/Main.hs
@@ -56,30 +56,32 @@ parseRuby (InfixCall left name_ right) = do
 
 parseRuby x = return x
 
-convert :: String -> StateT CodeState IO ()
-convert filename = do
-    contents <- liftIO $ lines <$> readFile filename
+convert :: String -> String -> StateT CodeState IO ()
+convert infile outfile = do
+    contents <- liftIO $ lines <$> readFile infile
     rubyLines <- forM (map Unresolved contents) parseRuby
     state <- get
     let headContents = toRuby <$> (state ^. headExtras)
         bodyContents = toRuby <$> (concatRuby rubyLines)
         newContents = headContents ++ bodyContents
-        newFilename = dropEnd 4 filename ++ ".rb"
   
-    liftIO $ writeFile (newFilename) (join "\n" newContents)
+    liftIO $ writeFile outfile (join "\n" newContents)
 
 printHelp = do
-    putStrLn "salmon adds some extra syntax to Ruby."
-    putStrLn "In particular, you can use `data Maybe = Nothing | Just a` now."
-    putStrLn "Give it a filename and it will generate _filename.rb."
+    putStrLn "Salmon adds some extra syntax to Ruby."
+    putStrLn "Usage: `salmon test.slm` (writes to test.rb)"
+    putStrLn "or `salmon input.slm output.rb` to write to output.rb"
 
 main = do
     args <- getArgs
     case args of
       ["-h"] -> printHelp
       ["--help"] -> printHelp
-      [filename] -> do
-        if (takeEnd 4 filename /= ".slm")
-          then putStrLn "Please give me a file with a .slm extension."
-          else runStateT (convert filename) defaultState >> return ()
+      [infile] -> do
+        if (takeEnd 4 infile /= ".slm")
+          then putStrLn "Please give me a file with a .slm extension, or specify a file to write to as the second argument."
+          else do
+            let outfile = dropEnd 4 infile ++ ".rb"
+            runStateT (convert infile outfile) defaultState >> return ()
+      [infile, outfile] -> runStateT (convert infile outfile) defaultState >> return ()
       _ -> printHelp
