5238711565e20aa889126b832f7b8e166c3de9ff
Aditya Bhargava <bluemangroupie@gmail.com>
2014-02-22 18:02:43 -0800

    working on some specs

--- spec/Main.hs
+++ spec/Main.hs
@@ -12,3 +12,21 @@ main = hspec $ do
   describe "Parsers" $ do
     it "should parse a data type" $ do
       "data Maybe val" `parseWith` classParser
+
+--     it "should parse parenthesis correctly" $ do
+--       "p (double(_) <$> (1..10))" `parseWith` ...
+      
+
+[
+("(1..10).map(&double(_))", "(1..10).map { |a| double(a) }"),
+("(1..10).map &double(_)", "(1..10).map { |a| double(a) }"), 
+("(1..10).map(&double)", "(1..10).map { |a| double(a) }"),
+("(1..10).map &double", "(1..10).map { |a| double(a) }"), 
+("map list &blk := list.map(&blk.call)", "def map(list, &blk)\n  list.map { |a| blk.call(a) }\nend"),
+("(1..10).map(&:even?)", "(1..10).map(&:even?)"),
+("(1..10).map(&add(:foo, _))", ("(1..10).map { |a| add(:foo, a) }"),
+("map(1..10, &double)", "map(1..10) { |a| double(a) }")
+]
+
+
+
